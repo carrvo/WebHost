558324b05cdbed944d4497c6193d864df1045e5e
carrvo <carrvo@gmail.com>
2023-12-14 17:06:01 -0700

    fix expectations

--- PowerShellUDPModule/UDP.Examples.Tests.ps1
+++ PowerShellUDPModule/UDP.Examples.Tests.ps1
@@ -54,42 +54,14 @@ Describe "UDP Listener" {
 		$reply |
             Select-Object -ExpandProperty Message |
             Should Be 'Hello test'
-        Send-UdpRequest -Port 11001 -Body $(@{Name='test'} | ConvertTo-Json)
+        Send-UdpRequest -Port 11001 -Body $(@{Name='test2'} | ConvertTo-Json)
         Start-Sleep -Seconds 1 # let the job output
 		[pscustomobject]$reply = Get-Job |
 			Receive-Job
 		$reply |
             Select-Object -ExpandProperty Message |
-            Should Be 'Hello test'
+            Should Be 'Hello test2'
         Get-Job -Name "indefinite requests" | Remove-Job -Force
-    }
-    It "does not require a body" {
-        Start-Job -Name "no body" -ScriptBlock {
-            Param(
-                $PSScriptRoot,
-                $uri
-            )
-            Import-Module $PSScriptRoot\PowerShellUDPModule.psd1
-            try {
-                New-UdpListener -Port 11002 |
-                    Wait-UdpRequest -Count 1 -PipelineVariable context |
-                    ForEach-Object {
-                        $request = $context.UTF8Payload | ConvertFrom-Json
-                        ([pscustomobject]@{Message="Hello $($request.Name)"}) | Write-Output -NoEnumerate
-                    }
-            } finally {
-                Get-UdpListener | Stop-UdpListener
-            }
-        } -ArgumentList $PSScriptRoot,$uri
-        Start-Sleep -Seconds 5 # let the job start listening
-        Send-UdpRequest -Port 11002 -Body $(@{Name='test'} | ConvertTo-Json)
-        Start-Sleep -Seconds 1 # let the job output
-		[pscustomobject]$reply = Get-Job |
-			Receive-Job
-		$reply |
-            Select-Object -ExpandProperty Message |
-            Should Be 'Hello test'
-        Get-Job -Name "no body" | Stop-Job -PassThru | Remove-Job
     }
     It "can respond to a request" {
         Start-Job -Name "response" -ScriptBlock {
